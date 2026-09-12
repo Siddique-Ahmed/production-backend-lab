8877e31265c857b2f5170bbf227dd45739f5494a
SiddiqueAhmed <153608121+Siddique-Ahmed@users.noreply.github.com>
2026-09-12 18:41:34 +0500

    feat: add environment configuration

---
+++ src/config/env.ts
@@ -0,0 +1,9 @@
+import "dotenv/config";
+import {z} from "zod";
+
+const envSchema = z.object({
+    PORT: z.string().default("5000"),
+    NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
+})
+
+export const env = envSchema.parse(process.env);
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
+import "dotenv/config";
 import { app } from "./app";
+import { env } from "./config/env";
 
-const PORT = 5000;
+const PORT = env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
